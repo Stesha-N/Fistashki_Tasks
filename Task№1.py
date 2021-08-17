92913e1d3fd6dbd7acb7b0980fab10c6a8636fff
Stesha-N <89044373+Stesha-N@users.noreply.github.com>
2021-08-18 00:20:37 +0300

    Update Task№1.py

--- Task№1.py
+++ Task№1.py
@@ -1,46 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ### Задание 1.
-
-# In[134]:
-
-
+#Задание1
 import pandas as pd
-df = pd.read_excel('C:/Users/User/Desktop/intro_dataset.xlsx')
-
-
-# In[135]:
-
-
-Total = df['Апельсины'].sum()
+df = pd.read_excel('intro_dataset.xlsx')
+total = df['Апельсины'].sum()
 print ("Продано всего апельсинов:", Total)
-
-
-# In[136]:
-
-
-Result = '\n'.join(list(df[df['Мандарины'] == df['Мандарины'].max()]['Дата']))#в случае нескольких месяцев с одинаковыми максимальными значениями 
+result = '\n'.join(list(df[df['Мандарины'] == df['Мандарины'].max()]['Дата']))#в случае нескольких месяцев с одинаковыми максимальными значениями 
 print ('Месяц, в котором было продано больше всего мандаринов:', Result)
-
-
-# ### Задание 2.
-
-# In[137]:
-
-
-df = pd.read_csv(r'C:\Users\User\Desktop\intro_dataset_2.txt', header=None)
+#Задание2
+df = pd.read_csv('intro_dataset_2.txt', header=None)
 new_df = df.T
-
-
-# In[141]:
-
-
 print ('Уникальные позиции:','\n'.join(list(new_df[0].unique())), sep = '\n')
-
-
-# In[140]:
-
-
 print('Сколько раз пользователи покупали тот или иной товар:', new_df[0].value_counts(), sep = '\n')
 
