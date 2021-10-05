abf623146a4c751922875e930c9c4f7f51bc263d
Stesha-N <89044373+Stesha-N@users.noreply.github.com>
2021-10-06 01:53:04 +0300

    Add files via upload

---
+++ Task№5.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[99]:
+
+
+import vk
+import time
+vk_api = vk.API(session,  v = '5.131')
+
+
+# In[100]:
+
+
+session = vk.Session(access_token='e41abe202b316df80c5061b661e0b44188a86c63819d11da1c08370d8b9862fbad3c7d65c812ac4396b0c')
+
+
+# In[104]:
+
+
+offset = 0
+member_counts = vk_api.groups.getMembers(group_id = "kult_patrul")['count']
+members_list = []
+while offset <= member_counts:
+    basa = vk_api.groups.getMembers(group_id = "kult_patrul", offset = offset, fields = 'country, city')['items']
+    for i in basa:
+        members_dict = {'URL':'https://vk.com/id'+ str(i['id']), 
+                    'ФИО':i['last_name'] + ' ' + i['first_name'],
+                    'Страна':i['country']['title'] if 'country' in i else 'Страна не указана',
+                    'Город':i['city']['title'] if 'city' in i else 'Город не указан'}
+        members_list.append(members_dict)
+    offset += 1000
+    time.sleep(0.5)
+print('Количество подписчиков:', len(members_list))
+print(members_list)
+
+
+# In[169]:
+
+
+def get_posts_and_comments(name_group):
+    offset = 0
+    posts_list = []
+    posts_counts = vk_api.wall.get(domain = name_group)['count']
+    while offset <= posts_counts:
+        baza = vk_api.wall.get(domain = name_group, offset = offset, extended=1, fields = 'text')['items']
+        for y in baza:
+            text_list = {'URL текста':'https://vk.com/'+ name_group + '?w=wall' + str(y['from_id'])+ '_' + str(y['id']), 
+                        'Текст':y['text'] if 'text' in y else 'Текста нет'}
+            time.sleep(0.5)
+            datas = vk_api.wall.getComments(owner_id = y['owner_id'], post_id = y['id'], extended=1)
+            comments_list = []
+            for t in range(len(datas['items'])):
+                comments_list.append(datas['items'][t]['text'])
+            text_list['Текст комментов'] = comments_list 
+            posts_list.append(text_list)
+        offset += 100
+        time.sleep(0.5)
+    return posts_list
+
+
+# In[170]:
+
+
+get_posts_and_comments('kult_patrul')
+
+
+# In[ ]:
+
+
+
+
